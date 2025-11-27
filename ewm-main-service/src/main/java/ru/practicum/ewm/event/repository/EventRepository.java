4a64361ea228c1695d4cdfd82f7934c10b4f1eaf
Vadim <uvarov.vadim2012@yandex.ru>
2025-11-27 17:44:15 +0300

    fix 11

--- ewm-main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
@@ -18,16 +18,16 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     Optional<Event> findByInitiatorIdAndId(Long userId, Long eventId);
 
-    @Query("SELECT e FROM Event e " +
-            "JOIN FETCH e.initiator i " +
-            "JOIN FETCH e.category c " +
+    @Query(value = "SELECT e FROM Event e " +
+            "JOIN FETCH e.initiator i " +  // Принудительно загружаем инициатора
+            "JOIN FETCH e.category c " +   // Принудительно загружаем категорию
             "WHERE (:users IS NULL OR i.id IN :users) " +
             "AND (:states IS NULL OR e.state IN :states) " +
             "AND (:categories IS NULL OR c.id IN :categories) " +
-            "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
-            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd)")
+            "AND (cast(:rangeStart as timestamp) IS NULL OR e.eventDate >= :rangeStart) " +
+            "AND (cast(:rangeEnd as timestamp) IS NULL OR e.eventDate <= :rangeEnd) ")
     Page<Event> findAllByAdminFilters(@Param("users") List<Long> users,
-                                      @Param("states") List<State> states,
+                                      @Param("states") List<String> states,
                                       @Param("categories") List<Long> categories,
                                       @Param("rangeStart") LocalDateTime rangeStart,
                                       @Param("rangeEnd") LocalDateTime rangeEnd,
@@ -55,6 +55,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             @Param("end") LocalDateTime end,
             Pageable pageable);
 
+    // === Public API: получение одного опубликованного события ===
     @Query("SELECT e FROM Event e WHERE e.id = :id AND e.state = :state")
     Optional<Event> findByIdAndState(@Param("id") Long id, @Param("state") State state);
 }
